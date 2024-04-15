c4db66fad9ab3576882649c3fc8cf16066f4f356
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-04-14 20:00:50 -0500

    debug

---
+++ README.md
@@ -0,0 +1,40 @@
+# ORM-E-commerce-Back-End
+
+## Description:
+This is a back end for my e-commerce website to successfully create, update, and delete data in a company's database.
+
+## Table of Contents:
+- [Usage](#Usage)
+- [Install](#Install)
+- [What I Learned](#What-I-Learned)
+- [Screenshot](#Screeshot)
+- [Walkthrough Video](#Walkthrough-Video)
+- [Author](#Author)
+- [Acknowledgments](#Acknowledgments)
+
+## Usage
+```md
+AS A manager at an internet retail company
+I WANT a back end for my e-commerce website that uses the latest technologies
+SO THAT my company can compete with other e-commerce companies
+```
+
+## Install
+1. Node.js https://nodejs.org/en/
+2. Sequelize https://www.npmjs.com/package/sequelize
+3. MySQL2 [Version 3.9.1] https://www.npmjs.com/package/mysql2
+4. Dotenv https://www.npmjs.com/package/dotenv
+6. Express https://www.npmjs.com/package/express-session
+
+## What I Learned
+- To build a database using MySQL to store sensitive data and connected to database using Sequelize. 
+
+## Screenshot
+The following picture shows application:
+![](./public/images/Screenshot.png)
+
+## Author
+Here is my <a href="https://github.com/arielRvaldez/ORM-E-commerce-Back-End.git" target="_blank">Github Repo Link</a>.
+
+## Acknowledgements
+- Calendy Tutor
---
+++ routes/api/category-routes.js
@@ -0,0 +1,77 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product, 
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'] }],
+    });
+    res.status(200).json(categories);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+  // be sure to include its associated Products
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // Category.findOne({
+  //   where: {
+  //     id: req.params.id,
+  //   },
+  //   include: [Product],
+  // })
+  //   .then((category) => res.json(category))
+  //   .catch((err) => res.status(400).json(err));
+  try {
+    const categoryId = req.params.id;
+    const categories = await Category.findOne(categoryId, {
+      include: [{ model: Product, 
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'] }],
+    });
+    if (!categories) {
+      res.status(404).json({ message: 'No category found with this id.' });
+      return;
+    }
+    res.status(200).json(categories);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+  // be sure to include its associated Products
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  Category.create(req.body)
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((category) => res.status(200).json(category))
+    .catch((err) => res.status(400).json(err));
+});
+
+module.exports = router;
